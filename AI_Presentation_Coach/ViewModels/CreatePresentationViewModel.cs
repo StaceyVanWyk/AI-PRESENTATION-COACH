5eff18ad46f6d7a3380f6a598ec95f710b07fac7
Stacey Van Wyk <svanwyk722@gmail.com>
2026-09-01 16:11:09 +0200

    Created a new folder ViewModels

---
+++ AI_Presentation_Coach/ViewModels/CreatePresentationViewModel.cs
@@ -0,0 +1,19 @@
+namespace AI_Presentation_Coach.ViewModels;
+
+public class CreatePresentationViewModel
+{
+    public bool IsValid(string? title, string? topic)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            return false;
+        }
+
+        if (string.IsNullOrWhiteSpace(topic))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
